156b2ecf146a86b0a0e3260f5e23b0b790512153
PuKhanal <PuKhanal@cs_labv29-305.cs.mum.edu>
2014-11-25 14:48:33 -0600

    fixed persistence issues

--- tbs/src/main/java/mum/cs490/tbs/model/CallingCodes.java
+++ tbs/src/main/java/mum/cs490/tbs/model/CallingCodes.java
@@ -19,7 +19,6 @@ import javax.persistence.Id;
 public class CallingCodes implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String country;
     private int code;
