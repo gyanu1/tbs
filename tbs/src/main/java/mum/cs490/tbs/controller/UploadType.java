c6e38871520805a6a781660ab3f4e7330d15ae47
PuKhanal <PuKhanal@cs_labv29-305.cs.mum.edu>
2014-11-26 11:22:44 -0600

    created Reader class

---
+++ tbs/src/main/java/mum/cs490/tbs/controller/UploadType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mum.cs490.tbs.controller;
+
+/**
+ *
+ * @author PuKhanal
+ */
+public enum UploadType {
+    rate,call_details,country_code
+}
